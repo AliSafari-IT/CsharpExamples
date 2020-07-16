30caeb477cec9a90073e104cde9516829f5d2e22
Ali SAFARI <58768873+AliSafari-IT@users.noreply.github.com>
2020-07-16 14:01:04 +0200

    Branch master (07/16/20 14:01:03): Csharp by Examples

---
+++ merge.sh
@@ -0,0 +1,51 @@
+printf "\n"
+echo "Csharp by Examples"
+printf "\n"
+echo "."
+echo ".."
+echo "..."
+echo "...."
+echo "....."
+echo "...... Merging with Master repository!"
+echo "....."
+echo "...."
+echo "..."
+echo ".."
+echo "."
+
+currentBranch=$(git branch | sed -n -e 's/^\* \(.*\)/\1/p')
+
+./push.sh
+
+echo "Get Master and Branch up-to-date"
+git checkout master
+git pull --rebase origin master
+git checkout $currentBranch
+git pull --rebase origin $currentBranch
+
+echo "Merge Branch on top of Master"
+git checkout $currentBranch
+git rebase master
+
+echo "Optional: If you run into Conflicts during the Rebase:"
+echo "First, resolve conflict in file. Then:"
+git add .
+git rebase --continue
+
+echo "Push your rebased Branch:"
+
+git push origin $currentBranch
+
+echo "Merge the branch into master"
+git checkout master
+git merge --no-ff $currentBranch
+git push origin master
+echo "Done."
+
+
+if [[ "$(git push --porcelain)" == *"Done"* ]]
+then
+  echo "Git merging with master was successful!"
+fi
+
+printf "\n \n"
